28cd78789b86f52144ec6ad2b0c901a25ec5f83e
Iain Cambridge <github@icambridge.me>
2014-12-07 19:08:25 +0000

    Add basic fetcher

--- dependency.go
+++ dependency.go
@@ -1,6 +1,7 @@
 package dependency
 
 import (
+	"fmt"
 	"github.com/deckarep/golang-set"
 )
 
@@ -17,22 +18,56 @@ func GetPackageNames(d Dependency) mapset.Set {
 		packages.Add(packageName)
 	}
 
-
 	return packages
 }
 
 type DependencyFetcher interface {
- 	Get(dependencyName string) (Dependency, error)
+	Get(dependencyName string) (map[string]Dependency, error)
 }
 
 type DependencyRepo struct {
-
+	DependencyNames mapset.Set
+	Dependencies    map[string]map[string]Dependency
+	Fetcher         DependencyFetcher
 }
-//
-// func (r DependencyRepo) GetAll(d Dependency) mapset.Set {
-//
-//
-//
-//
-// 	return input := mapset.NewSet()
-// }
+
+func (r DependencyRepo) GetAll(dependencies mapset.Set) {
+
+	f := dependencies.Intersect(r.DependencyNames)
+
+	if f.Cardinality() == dependencies.Cardinality() {
+		return
+	}
+
+	subDependencies := mapset.NewSet()
+	for _, nameI := range dependencies.ToSlice() {
+		name := fmt.Sprintf("%s", nameI)
+		r.DependencyNames.Add(name)
+		dm, err := r.Fetcher.Get(name)
+
+		if err != nil {
+			panic(err)
+		}
+
+		for _, sd := range dm {
+			for packageName, _ := range sd.Requires {
+				subDependencies.Add(packageName)
+			}
+		}
+
+	}
+	r.GetAll(subDependencies)
+}
+
+
+
+func GetNewRepo(fetcher DependencyFetcher) DependencyRepo {
+	dr := DependencyRepo{
+
+		DependencyNames: mapset.NewSet(),
+		Dependencies:    map[string]map[string]Dependency{},
+		Fetcher:         fetcher,
+	}
+
+	return dr
+}
--- dependency_test.go
+++ dependency_test.go
@@ -1,10 +1,11 @@
 package dependency
 
 import (
-    "github.com/deckarep/golang-set"
-    "testing"
-    )
-    
+	"errors"
+	"github.com/deckarep/golang-set"
+	"testing"
+)
+
 func Test_GetPackageNames(t *testing.T) {
 	root := Dependency{
 		Name: "App",
@@ -15,7 +16,7 @@ func Test_GetPackageNames(t *testing.T) {
 	}
 
 	actual := GetPackageNames(root)
-    expected := mapset.NewSet()
+	expected := mapset.NewSet()
 	expected.Add("behat/mink-symfony")
 	expected.Add("behat/mink-ext")
 
@@ -24,23 +25,77 @@ func Test_GetPackageNames(t *testing.T) {
 	}
 }
 
-// func Test_GetAllDependencies(t *testing.T) {
-//
-//     root := Dependency{
-//         Name: "App",
-//         Requires: map[string]string{
-//             "behat/mink-symfony": "~1.1",
-//             "behat/mink-ext":     "~1.1",
-//             },
-//         }
-//
-// }
-//
-//
-// type MockFetcher struct {
-//
-// }
-//
-// func (mf MockFetcher) Get(dependencyName string) (Dependency, error) {
-//
-// }
+func Test_GetAllDependencies(t *testing.T) {
+	packages := mapset.NewSet()
+	packages.Add("behat/mink-ext")
+	packages.Add("behat/mink-symfony")
+
+	f := MockFetcher{}
+
+	r := GetNewRepo(f)
+
+	r.GetAll(packages)
+
+	expected := mapset.NewSet()
+	expected.Add("behat/mink-ext")
+	expected.Add("behat/mink-symfony")
+	expected.Add("behat/mink")
+
+	if !r.DependencyNames.Equal(expected) {
+		t.Errorf("Expected %s, but got %s", expected, r.DependencyNames)
+	}
+}
+
+type MockFetcher struct {
+}
+
+func (mf MockFetcher) Get(dependencyName string) (map[string]Dependency, error) {
+
+	mink := map[string]Dependency{
+		"1.6.0": Dependency{
+			Name:    "behat/mink",
+			Version: "1.6.0",
+		},
+		"1.5.1": Dependency{
+			Name:    "behat/mink",
+			Version: "1.5.1",
+		},
+	}
+
+	minkExt := map[string]Dependency{
+		"1.4.1": Dependency{
+			Name:    "behat/mink-ext",
+			Version: "1.2.5",
+			Requires: map[string]string{
+				"behat/mink": "~1.5",
+			},
+		},
+	}
+
+	minkSymfonyBrowser := map[string]Dependency{
+		"1.2.0": Dependency{
+			Name:    "behat/mink-symfony",
+			Version: "1.2.5",
+			Requires: map[string]string{
+				"behat/mink": "~1.6",
+			},
+		},
+		"1.1.0": Dependency{
+			Name:    "behat/mink-symfony",
+			Version: "1.2.5",
+			Requires: map[string]string{
+				"behat/mink": "~1.5",
+			},
+		},
+	}
+	switch dependencyName {
+	case "behat/mink-ext":
+		return minkExt, nil
+	case "behat/mink-symfony":
+		return minkSymfonyBrowser, nil
+	case "behat/mink":
+		return mink, nil
+	}
+
+	return nil, errors.New("Not found")
+}
