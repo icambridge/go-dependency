c2d5e4889c3bc56e20d45c1782996be0d5a9a6a8
Iain Cambridge <github@icambridge.me>
2014-12-07 16:53:36 +0000

    Start again with base solve

--- solver.go
+++ solver.go
@@ -8,96 +8,20 @@ type Solver struct {
 	Packages map[string]map[string]Dependency
 }
 
-func (s Solver) Solve() map[string]string {
+func (s Solver) Solve(root Dependency) map[string]string {
 
 	required := map[string]string{}
+	// found :=
+	for packageName, rule := range root.Requires {
+		cg := version.NewConstrainGroupFromString(rule)
+		versions := s.Packages[packageName]
+		for versionNum, _ := range versions {
+			if cg.Match(versionNum) {
+				required[packageName] = versionNum
+				break
+			}
+		}
+	}
 
 	return required
-}
-
-func (s Solver) findUniqueRules(dependencies map[string]Dependency) map[string]map[string][]string {
-
-	rules := map[string]map[string][]string{}
-
-	for version, dependency := range dependencies {
-		for name, rule := range dependency.Requires {
-			_, nameExists := rules[name]
-
-			if !nameExists {
-				rules[name] = map[string][]string{}
-			}
-
-			_, ruleExists := rules[name][rule]
-
-			if !ruleExists {
-				rules[name][rule] = []string{}
-			}
-
-			rules[name][rule] = append(rules[name][rule], version)
-
-		}
-	}
-
-	return rules
-}
-
-func (s Solver) mergeRules(rules map[string]map[string]map[string][]string) map[string]map[string][]string {
-
-	mergedRules := map[string]map[string][]string{}
-
-	for name, dRules := range rules {
-
-		for dependencyName, pdRules := range dRules {
-
-			_, nameExists := mergedRules[dependencyName]
-
-			if !nameExists {
-				mergedRules[dependencyName] = map[string][]string{}
-			}
-			for rule, _ := range pdRules {
-
-				_, ruleExists := mergedRules[dependencyName][rule]
-
-				if !ruleExists {
-					mergedRules[dependencyName][rule] = []string{}
-				}
-
-				mergedRules[dependencyName][rule] = append(mergedRules[dependencyName][rule], name)
-			}
-		}
-	}
-
-	return mergedRules
-}
-
-func (s Solver) getSuggestionRule(packagesRequiring []string, rules map[string][]string) string {
-
-	for rule, packages := range rules {
-		if len(packagesRequiring) == len(packages) {
-			return rule
-		}
-	}
-
-	return "nil"
-}
-
-func allPass(rules map[string][]string, versions map[string]Dependency) bool {
-
-	ruleCount := len(rules)
-	for versionNum, _ := range versions {
-
-		counter := 0
-		for rule, _ := range rules {
-			cg := version.NewConstrainGroupFromString(rule)
-
-			if cg.Match(versionNum) {
-				counter++
-			}
-		}
-
-		if counter == ruleCount {
-			return true
-		}
-	}
-	return false
 }
--- solver_test.go
+++ solver_test.go
@@ -1,8 +1,8 @@
 package dependency
 
 import (
-	set "github.com/deckarep/golang-set"
-	"reflect"
+	// set "github.com/deckarep/golang-set"
+	// "reflect"
 	"testing"
 )
 
@@ -52,201 +52,25 @@ func Test_Gets_Correct(t *testing.T) {
 		"behat/mink-symfony": minkSymfonyBrowser,
 	}
 
-	s := Solver{packages}
-	required := s.Solve()
-
-	if minkV := "1.5.1"; minkV != required["behat/mink"] {
-		t.Errorf("Expected to require %v but got %v", minkV, required["behat/mink"])
-		return
+	root := Dependency{
+		Name: "App",
+		Requires: map[string]string{
+			"behat/mink-symfony": "~1.1",
+			"behat/mink-ext":     "~1.1",
+		},
 	}
+
+	s := Solver{packages}
+	required := s.Solve(root)
+
 
 	if minkExtV := "1.4.1"; minkExtV != required["behat/mink-ext"] {
-		t.Errorf("Expected to require %v but got %v", minkExtV, required["behat/mink"])
+		t.Errorf("Expected to require %v but got %v", minkExtV, required["behat/mink-ext"])
 		return
 	}
 
-	if minkSymfonyV := "1.1.0"; minkSymfonyV != required["behat/mink-symfony"] {
-		t.Errorf("Expected to require %v but got %v", minkSymfonyV, required["behat/mink"])
+	if minkSymfonyV := "1.2.0"; minkSymfonyV != required["behat/mink-symfony"] {
+		t.Errorf("Expected to require %v but got %v", minkSymfonyV, required["behat/mink-symfony"])
 		return
 	}
-}
-
-func Test_Find_Unique_Rules(t *testing.T) {
-
-	minkSymfonyBrowser := map[string]Dependency{
-		"1.2.0": Dependency{
-			Name:    "behat/mink-symfony",
-			Version: "1.2.5",
-			Requires: map[string]string{
-				"behat/mink": "~1.6",
-			},
-		},
-		"1.1.0": Dependency{
-			Name:    "behat/mink-symfony",
-			Version: "1.2.5",
-			Requires: map[string]string{
-				"behat/mink": "~1.5",
-			},
-		},
-	}
-	s := Solver{}
-	actual := s.findUniqueRules(minkSymfonyBrowser)
-
-	expected := map[string]map[string][]string{
-		"behat/mink": map[string][]string{
-			"~1.5": []string{"1.1.0"},
-			"~1.6": []string{"1.2.0"},
-		},
-	}
-
-	if !reflect.DeepEqual(actual, expected) {
-		t.Errorf("Expected to %v, got %v", actual, expected)
-	}
-}
-
-func Test_Merge_Rules(t *testing.T) {
-
-	name := "behat/mink"
-	symfony := map[string]map[string][]string{
-		name: map[string][]string{
-			"~1.5": []string{"1.1.0"},
-			"~1.6": []string{"1.2.0"},
-		},
-	}
-	minkExt := map[string]map[string][]string{
-		name: map[string][]string{
-			"~1.5": []string{"2.1.0"},
-		},
-	}
-
-	allRules := map[string]map[string]map[string][]string{
-		"behat/mink-ext":     minkExt,
-		"behat/mink-symfony": symfony,
-	}
-
-	s := Solver{}
-	actual := s.mergeRules(allRules)
-
-	if len(actual) != 1 {
-		t.Errorf("Expected to be one got %v", len(actual))
-	}
-
-	if len(actual[name]) != 2 {
-		t.Errorf("Expected to be two")
-	}
-
-	if len(actual[name]["~1.5"]) != 2 {
-		t.Errorf("Expected to be two")
-	}
-	expected15 := []string{"behat/mink-ext", "behat/symfony"}
-	Sx := NewSetFromStringSlice(actual[name]["~1.5"])
-	Sy := NewSetFromStringSlice(expected15)
-
-	if Sx.Equal(Sy) {
-		t.Errorf("Got %v, expected %v", actual[name]["~1.5"], expected15)
-	}
-	expected16 := []string{"behat/symfony"}
-	Sx = NewSetFromStringSlice(actual[name]["~1.6"])
-	Sy = NewSetFromStringSlice(expected16)
-	if len(actual[name]["~1.6"]) != 1 {
-		t.Errorf("Expected to be one")
-	}
-
-	if Sx.Equal(Sy) {
-		t.Errorf("Got %v, expected %v", actual[name]["~1.6"], expected16)
-	}
-}
-
-func Test_SuggestPackage_15(t *testing.T) {
-
-	requiringPackages := []string{"behat/symfony", "behat/mink-ext"}
-
-	rules := map[string][]string{
-		"~1.6": []string{"behat/symfony"},
-		"~1.5": []string{"behat/symfony", "behat/mink-ext"},
-	}
-
-	s := Solver{}
-
-	actual := s.getSuggestionRule(requiringPackages, rules)
-	expected := "~1.5"
-	if expected != actual {
-		t.Errorf("Expected to get rule %v, but got %v", expected, actual)
-	}
-}
-
-func Test_SuggestPackage_15_With_Wildcard(t *testing.T) {
-
-	requiringPackages := []string{"behat/symfony", "behat/mink-ext", "icambridge/demo"}
-
-	rules := map[string][]string{
-		"~1.6": []string{"behat/symfony"},
-		"~1.5": []string{"behat/symfony", "behat/mink-ext"},
-		"*":    []string{"icambridge/demo"},
-	}
-
-	s := Solver{}
-	actual := s.getSuggestionRule(requiringPackages, rules)
-	expected := "~1.5"
-	if expected != actual {
-		t.Errorf("Expected to get rule %v, but got %v", expected, actual)
-	}
-}
-
-func Test_All_Pass_False(t *testing.T) {
-
-	rules := map[string][]string{
-		"~1.6": []string{"behat/symfony"},
-		">1.5": []string{"behat/symfony", "behat/mink-ext"},
-		"*":    []string{"icambridge/demo"},
-	}
-
-	mink := map[string]Dependency{
-		"2.6.0": Dependency{
-			Name:    "behat/mink",
-			Version: "2.6.0",
-		},
-		"1.5.1": Dependency{
-			Name:    "behat/mink",
-			Version: "1.5.1",
-		},
-	}
-
-	actual := allPass(rules, mink)
-
-	if actual {
-		t.Errorf("Expected not to be true")
-	}
-}
-
-func Test_All_Pass_True(t *testing.T) {
-
-	rules := map[string][]string{
-		">1.5": []string{"behat/symfony", "behat/mink-ext"},
-		"*":    []string{"icambridge/demo"},
-	}
-
-	mink := map[string]Dependency{
-		"2.6.0": Dependency{
-			Name:    "behat/mink",
-			Version: "2.6.0",
-		},
-		"1.5.1": Dependency{
-			Name:    "behat/mink",
-			Version: "1.5.1",
-		},
-	}
-
-	actual := allPass(rules, mink)
-	if !actual {
-		t.Errorf("Expected to be true")
-	}
-}
-
-func NewSetFromStringSlice(s []string) set.Set {
-	a := set.NewSet()
-	for _, item := range s {
-		a.Add(item)
-	}
-	return a
 }
